23ecd6e46d8207939769d5764dd5bd58b29046ef
roosvell-velandia_adlover <roosvell.velandia@AvalDigitalLabs.com>
2023-10-08 17:13:21 -0500

    design patterns (adapter)

---
+++ design_patterns/adapter.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+// adapta el comportamiento de una clase para no implementar completamente una interface
+// interface with a method
+type Payment interface {
+	Pay()
+}
+
+// example when the method is implemented as original interface
+type CashPayment struct{}
+
+func (CashPayment) Pay() {
+	fmt.Println("Payment using cash")
+}
+
+func ProcessPayment(p Payment) {
+	p.Pay()
+}
+
+// another instance changing interface implementation
+type BankPayment struct{}
+
+func (BankPayment) Pay(bankAccount int) {
+	fmt.Printf("Payment using banc account %d  \n", bankAccount)
+}
+
+// new object with extra parameters
+type BankPaymentAdapter struct {
+	BankPayment *BankPayment
+	bankAccount int
+}
+
+// new implementation in the method
+func (bpa *BankPaymentAdapter) Pay() {
+	bpa.BankPayment.Pay(bpa.bankAccount)
+}
+
+func main() {
+	cash := &CashPayment{}
+	ProcessPayment(cash)
+	//bank := &BankPayment{}
+	//ProcessPayment(bank)
+	// create the adapter object
+	bpa := &BankPaymentAdapter{
+		bankAccount: 5,
+		BankPayment: &BankPayment{},
+	}
+	// use the adapter implementation
+	ProcessPayment(bpa)
+}
